50991b18b7d36e2723912431cdac2789bd276d55
Montasir Mihad <ahmedmihad962@gmail.com>
2025-05-01 23:23:03 +0600

    refactor

--- src/app/DB/index.ts
+++ src/app/DB/index.ts
@@ -1,22 +1,22 @@
-import config from "../config";
-import { USER_ROLE } from "../modules/user/user.const";
-import { User } from "../modules/user/user.model";
+// import config from "../config";
+// import { USER_ROLE } from "../modules/user/user.const";
+// import { User } from "../modules/user/user.model";
 
-const superUser = {
-  id: "0001",
-  email: "ahmedmihad962@gmail.com",
-  password: config.super_admin_password,
-  needsPasswordChange: false,
-  role: USER_ROLE.superAdmin,
-  status: "in-progress",
-  isDeleted: false,
-};
+// const superUser = {
+//   id: "0001",
+//   email: "ahmedmihad962@gmail.com",
+//   password: config.super_admin_password,
+//   needsPasswordChange: false,
+//   role: USER_ROLE.superAdmin,
+//   status: "in-progress",
+//   isDeleted: false,
+// };
 
-const seedSuperAdmin = async () => {
-  const isSuperAdminExist = await User.findOne({ role: USER_ROLE.superAdmin });
-  if (!isSuperAdminExist) {
-    await User.create(superUser);
-  }
-};
+// const seedSuperAdmin = async () => {
+//   const isSuperAdminExist = await User.findOne({ role: USER_ROLE.superAdmin });
+//   if (!isSuperAdminExist) {
+//     await User.create(superUser);
+//   }
+// };
 
-export default seedSuperAdmin;
+// export default seedSuperAdmin;
